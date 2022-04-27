e636e8df30fbc27ebdf99ff8947f897c446260da
jddeines <jacob.deines@jacks.sdstate.edu>
2022-04-26 23:32:26 -0500

    Finish sections 10 and 11
    It looks much better now!

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging, os
 from logging.handlers import SMTPHandler, RotatingFileHandler
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,6 +14,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
